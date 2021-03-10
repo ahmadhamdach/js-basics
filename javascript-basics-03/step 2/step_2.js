de05f29e88a94cb15a8c725da847d823a8429bbf
Hamdash <ahmadhamdach1997@gmail.com>
2021-03-10 11:09:31 +0200

    step-2

--- javascript-basics-03/step 2/step_2.js
+++ javascript-basics-03/step 2/step_2.js
@@ -2,12 +2,12 @@ var s=document.querySelector("#show");
 var h=document.querySelector("#hide");
 var t=document.querySelector("#texte");
 const showhide=(i)=>{
-    if(i=="sh"){
+    if(i=="sw"){
         t.style.visibility="visible";
     }
     else if(i=="hi"){
         t.style.visibility="hidden";
     }
 }
-s.addEventListener("click",()=>{showhide("sh")});
+s.addEventListener("click",()=>{showhide("sw")});
 s.addEventListener("click",()=>{showhide("hi")});
